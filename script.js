abf207529b969baa3f430a73f9dde8999c010545
Natalia Varela Grajales <est.natalia.varela@unimilitar.edu.co>
2026-04-06 18:53:45 -0500

    Inicializo canvas y contexto de dibujo

---
+++ script.js
@@ -0,0 +1,10 @@
+/**
+ * Elemento canvas donde se dibujara la linea
+ * @type {HTMLCanvasElement}
+ */
+const canvas = document.getElementById("canvas");
+
+/**
+ * Contexto 2D para dibujar en el canvas
+ */
+const ctx = canvas.getContext("2d");
