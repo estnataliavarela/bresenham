abd411a4b545ce8e19631572dd9c53af59cdc974
Natalia Varela Grajales <est.natalia.varela@unimilitar.edu.co>
2026-04-06 19:10:46 -0500

    Creo funcion dibujar para manejar el evento del boton

--- script.js
+++ script.js
@@ -8,3 +8,10 @@ const canvas = document.getElementById("canvas");
  * Contexto 2D para dibujar en el canvas
  */
 const ctx = canvas.getContext("2d");
+/**
+ * Funcion principal que se ejecuta al presionar el boton
+ * Controla el flujo del programa
+ */
+function dibujar() {
+    console.log("Boton presionado");
+}
