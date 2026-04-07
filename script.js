2ae0c014e82910b84b8d08bb646c3364221ef030
Natalia Varela Grajales <est.natalia.varela@unimilitar.edu.co>
2026-04-06 19:35:07 -0500

    Implemento ciclo principal de Bresenham

--- script.js
+++ script.js
@@ -70,3 +70,21 @@ let dy = Math.abs(y1 - y0);
 let sx = (x0 < x1) ? 1 : -1;
 let sy = (y0 < y1) ? 1 : -1;
 let err = dx - dy;
+while (true) {
+
+    plot(x0, y0);
+
+    if (x0 === x1 && y0 === y1) break;
+
+    let e2 = 2 * err;
+
+    if (e2 > -dy) {
+        err -= dy;
+        x0 += sx;
+    }
+
+    if (e2 < dx) {
+        err += dx;
+        y0 += sy;
+    }
+}
