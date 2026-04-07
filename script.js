f5fe17fabbfe6082d9756935961eab2f44896b87
Natalia Varela Grajales <est.natalia.varela@unimilitar.edu.co>
2026-04-06 19:33:36 -0500

    Agrego calculo de variables del algoritmo de Bresenham

--- script.js
+++ script.js
@@ -65,3 +65,8 @@ function dibujarEscala() {
 function bresenham(x0, y0, x1, y1, plot) {
 
 }
+let dx = Math.abs(x1 - x0);
+let dy = Math.abs(y1 - y0);
+let sx = (x0 < x1) ? 1 : -1;
+let sy = (y0 < y1) ? 1 : -1;
+let err = dx - dy;
